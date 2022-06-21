7f4c141bd4009595b666f0f7aafd22f2cac6d8bf
mahmoudT95 <mahmoud.t1995@gmail.com>
2022-06-21 17:17:59 +0200

    1st

--- js/app.js
+++ js/app.js
@@ -23,43 +23,47 @@
  * 
 */
 const theList = document.getElementById('navbar__list') // select navabr list by it is id
-const section = document.getElementsByTagName('section'); // select all sections by there tag
+const sections = [...document.getElementsByTagName('section')]; // select all sections by there tag
+const section = document.querySelectorAll('.landing__container');// select container 
+const mybutton = document.getElementById("myBtn");// select btn
 /**
  * End Global Variables
  * Start Helper Functions
  * 
  * 
 */
-//function jumpTo() {
-    //const element = document.getElementById(navMenu());
-  //  element.scrollIntoView();
-  //}
+
 // Build the navigation menu
     function navMenu(){
-        for(let i=1; i<5; i++){
+        sections.forEach(section =>{
+            const secId = section.getAttribute('id')// get id to add it later to href
+            const secname = section.getAttribute('data-nav')// get data nav to give every li inner text
             const newli= document.createElement('li') // create new li item
             const linkName = document.createElement('a'); // create new anchor
             linkName.classList.add('menu__link');//add class to anchor
-            linkName.setAttribute("href", `#section${i}`)//add href so we can use event listenr to go to section that contain section id
-            linkName.innerText = `Section ${i}`;//anchor inner text it will be added dynamically by for loop 
-           theList.appendChild(newli);
-           newli.appendChild(linkName);
-           // event listner will act on active anchor once it click it will go to section related to active element
-           linkName.addEventListener('click', function () {
-            const element = document.getElementById(`section${i}`);
-          linkName.scrollIntoView({
-            behavior: 'smooth'
-          });
-           
-          });
-        }
-        
+            linkName.setAttribute("href", `#section${secId}`)//add href so we can use event listenr to go to section that contain section id
+            linkName.innerText = `Section ${secname}`;//anchor inner text it will be added dynamically by for loop 
+            // nesting
+           newli.appendChild(linkName); // add anchors that stored in linkname to li stored in newLi
+           theList.appendChild(newli);  // add li to navbar list 
+           // Scroll to section on link click 
+           linkName.addEventListener('click' , fun=> {
+            fun.preventDefault();// prevent scroll by href 
+           section.scrollIntoView({behavior: "smooth"}); // add scrool by clicking on element 
 
-      
-    }
-navMenu();
+          
+           })
+        })
+}
+////https://developer.mozilla.org/en-US/docs/Web/API/Window/DOMContentLoaded_event
+window.addEventListener('DOMContentLoaded', (event) => {
+    
+    navMenu();
+});
 /**
  * End Helper Functions
+ * 
+ * 
  * Begin Main Functions
  * 
 */
@@ -67,20 +71,44 @@ navMenu();
 // build the nav
 
 
-// Add class 'active' to section when near top of viewport
+//add amd  remove class 'your-active-class' 
 
-
-// Scroll to anchor ID using scrollTO event
-
+function makeActive(){                     
+    sections.forEach(section =>{
+    const view= section.getBoundingClientRect();
+    if(view.top <=150 && view.bottom >=150){ // this will check if class on view to add active
+        section.classList.add("your-active-class");
+        document.querySelector(`.landing__container`).classList.add("your-active-class");
+    } else {// if not in viwe will rmove class active
+        
+        section.classList.remove("your-active-class");
+        document.querySelector(`.landing__container`).classList.remove("your-active-class");
+        }
+    })
+}
 
 /**
  * End Main Functions
  * Begin Events
  * 
 */
-
-// Build menu 
-
-// Scroll to section on link click
-
 // Set sections as active
+document.addEventListener("scroll", () => {
+    makeActive();
+  }); 
+
+  ///btn function to diplay it after user scroll 
+  function scrollFunction() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop >200) {
+      mybutton.style.display = "inline-block";
+    } else {
+      mybutton.style.display = "none";
+    }
+  }
+  
+  // When the user clicks on the button, scroll to the top of the document
+  function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;}
+
+    window.onscroll = function() {scrollFunction()};
