d71a6b139ac0d3fd9ad67986b33945e73635bb2b
mahmoudT95 <mahmoud.t1995@gmail.com>
2022-06-23 21:02:36 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -124,9 +124,9 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  window.scrollTo({ top: 0, behavior: 'smooth' })
-  document.documentElement.scrollTop = 0
-  theList.style.display = 'block'
+  window.scrollTo({ top: 0, behavior: 'smooth'})
+  
+  
 }
 
 window.onscroll = function () {
